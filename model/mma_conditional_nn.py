fixes scaling error

model/mma_conditional_nn.py
@@ -50,7 +50,7 @@ class SymmetricModelWrapper(object):
     def _get_X(self, train_df, test_df):
         X_temp = np.concatenate([train_df[self.fighter_cols], 
                                  train_df[self.opponent_cols]])
-        scale_ = (X_temp**2).mean(0)
+        scale_ = np.sqrt((X_temp**2).mean(0))
         pca = PCA(n_components=self.n_pca, whiten=True)
         pca.fit(X_temp / scale_)
         

model/mma_hier_log_reg_stan.py
@@ -72,7 +72,7 @@ class HierPcaSymmetricModel(PcaSymmetricModel):
             self._load_stan_model()
         if not feat_cols:
             feat_cols = self.feat_cols
-        scale_ = (train_df[feat_cols]**2).mean(0)
+        scale_ = np.sqrt((train_df[feat_cols]**2).mean(0))
         self.scale_ = scale_
         X_train = train_df[feat_cols] / scale_
         X_test = test_df[feat_cols] / scale_

model/mma_log_reg_stan.py
@@ -99,7 +99,7 @@ class SimpleSymmetricModel(BaseStanModel):
             feat_cols = self.feat_cols
         if self.stan_model is None:
             self._load_stan_model()
-        scale_ = (train_df[feat_cols]**2).mean(0)
+        scale_ = np.sqrt((train_df[feat_cols]**2).mean(0))
         self.scale_ = scale_
         X_train = train_df[feat_cols] / scale_
         X_test = test_df[feat_cols] / scale_
@@ -140,7 +140,7 @@ class PcaSymmetricModel(SimpleSymmetricModel):
             feat_cols = self.feat_cols
         if self.stan_model is None:
             self._load_stan_model()
-        scale_ = (train_df[feat_cols]**2).mean(0)
+        scale_ = np.sqrt((train_df[feat_cols]**2).mean(0))
         self.scale_ = scale_
         X_train = train_df[feat_cols] / scale_
         X_test = test_df[feat_cols] / scale_